4091a9778068dc954f44d1790f56a2055cf70bdb
DiegoOBritto <diegoob71@gmail.com>
2021-11-29 21:49:15 -0300

    Criando Projeto React e adicionando Home

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1>Home</h1>
+            <img src="https://i.imgur.com/fkopnQH.jpeg" alt="Imagem Tela Inicial" />;
+        </>
+
+    )
+
+}
+export default Home;
